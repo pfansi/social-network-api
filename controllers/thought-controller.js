b5c690abff1d53d1a9c15330b2b0a54c26b4d6cd
pfansiW <fancyfreelance76@gmail.com>
2022-03-29 02:49:25 +0100

    modified the deleteThought function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -61,34 +61,46 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
-  // delete thought
-  deleteThought(req, res) {
-    Thought.findOneAndRemove({ _id: req.params.thoughtId })
-      .then((dbThoughtData) => {
-        if (!dbThoughtData) {
-          return res.status(404).json({ message: "No thought with this id!" });
-        }
 
-        // remove thought id from user's `thoughts` field
-        return User.findOneAndUpdate(
-          { thoughts: req.params.thoughtId },
-          { $pull: { thoughts: req.params.thoughtId } },
-          { new: true }
-        );
-      })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          return res
-            .status(404)
-            .json({ message: "Thought created but no user with this id!" });
+  // remove thought
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then((deletedThought) => {
+        if (!deletedThought) {
+          return res.status(200).json({ message: "Thought deleted" });
         }
-        res.json({ message: "Thought successfully deleted!" });
       })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+      .catch((err) => res.json(err));
   },
+
+  // // delete thought
+  // deleteThought(req, res) {
+  //   Thought.findOneAndRemove({ _id: req.params.thoughtId })
+  //     .then((dbThoughtData) => {
+  //       if (!dbThoughtData) {
+  //         return res.status(404).json({ message: "No thought with this id!" });
+  //       }
+
+  //       // remove thought id from user's `thoughts` field
+  //       return User.findOneAndUpdate(
+  //         { thoughts: req.params.thoughtId },
+  //         { $pull: { thoughts: req.params.thoughtId } },
+  //         { new: true }
+  //       );
+  //     })
+  //     .then((dbUserData) => {
+  //       if (!dbUserData) {
+  //         return res
+  //           .status(404)
+  //           .json({ message: "Thought created but no user with this id!" });
+  //       }
+  //       res.json({ message: "Thought successfully deleted!" });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       res.status(500).json(err);
+  //     });
+  // },
 
   // add a reaction to a thought
   addReaction(req, res) {
